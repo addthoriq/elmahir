12a89aa2020238b57f62ac7e05cf8cb69a761da9
addthoriq <alvarro.thoriq@gmail.com>
2020-01-07 09:38:10 +0700

    delete file auth kemaren

--- resources/views/auth/student.blade.php
+++
@@ -1,9 +0,0 @@
-<form class="mt-5 mb-5 login-input" action="">
-    <div class="form-group">
-        <input type="email" name="email" class="form-control" placeholder="Email">
-    </div>
-    <div class="form-group">
-        <input type="password" name="password" class="form-control" placeholder="Password">
-    </div>
-    <button class="btn login-form__btn submit w-100">Masuk sebagai Siswa</button>
-</form>
